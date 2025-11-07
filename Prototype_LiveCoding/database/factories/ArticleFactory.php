67b6509cdd1c4e5e932832f9b691006e3015ff78
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2025-11-07 09:03:18 +0000

    Add initial prototype from live-coding session. Closes #6

---
+++ Prototype_LiveCoding/database/factories/ArticleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'title' => $title = fake()->sentence(6),
+            'slug' => Str::slug($title),
+            'content' => fake()->paragraphs(3, true),
+        ];
+    }
+}
