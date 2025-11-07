166895c82ba64f7463f5425328de241e62491105
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2025-11-07 15:55:29 +0000

    Refactor code structure for improved readability and maintainability

--- Blog_Ouarzazate/app/Models/User.php
+++ Blog_Ouarzazate/app/Models/User.php
@@ -2,47 +2,30 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'name', 'email', 'password', 'role_id'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    public function articles() {
+        return $this->hasMany(Article::class, 'author_id');
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function favorites() {
+        return $this->belongsToMany(Article::class, 'favorites', 'user_id', 'article_id')->using(Favorite::class);
     }
 }
